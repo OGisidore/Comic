600f0c857481011db996273eb3c1b12448c71bd2
OGisidore <106089573+OGisidore@users.noreply.github.com>
2025-02-17 07:04:28 +0100

    Update settings.py

--- Comics/settings.py
+++ Comics/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-66h#mgl#yll-^^#faxbi((p6*-mh(e!)wql&=d+0950tg7f8b-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 PORT = os.getenv("PORT",8080)
-ALLOWED_HOSTS = ["comicme.netlify.app", "localhost", "127.0.0.1", "[::1]"]
+ALLOWED_HOSTS = ["comic-8dve.onrender.com","comicme.netlify.app","localhost","127.0.0.1","[::1]"]
 
 
 CORS_ALLOW_CREDENTIALS = True
@@ -49,6 +49,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8080",
     "http://localhost:3001",
+    "https://comic-8dve.onrender.com"
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 
